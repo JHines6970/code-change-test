62fcf5238431ad63cd605e9d0ac759f549f8b29d
JHines6970 <jhines6970@sohamcollege.org.uk>
2014-05-08 11:58:38 +0100

    Update of GreenBottles.py

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,10 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+Bottles=int(input("Enter number of bottles:"))
+while Bottles != 0:
+    print((str(Bottles)+" green bottles, hanging on the wall"))
+    print((str(Bottles)+" green bottles, hanging on the wall"))
+    print("And if 1 green bottle should acidentally fall,")
+    Bottles=Bottles-1
+    time.sleep(2)
+print()
+print("There will be no green bottles, hanging on the wall.")
